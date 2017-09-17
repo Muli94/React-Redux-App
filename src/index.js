0fbc5f26afdf8006d7edb386b4bad730ce32611e
Karol Mularczyk <karol.mularcz@gmail.com>
2017-09-17 12:57:26 +0200

    started the app, make action and reducer

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
 
 import reducers from './reducers';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
